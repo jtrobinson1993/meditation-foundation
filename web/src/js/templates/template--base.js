8bd46b374b01886c3b458fce3e53f7d34a8d8fb3
Jarrod Robinson <jrobinson@proteus.co>
2019-02-02 18:55:56 -0600

    desktop adjustments, finish menu

--- web/src/js/templates/template--base.js
+++ web/src/js/templates/template--base.js
@@ -6,9 +6,16 @@ const mainMenu = document.querySelector('#main-menu');
 const menuButtonText = document.querySelectorAll('#main-menu-toggle .screen-reader-only')[0];
 
 menuButton.addEventListener('click', () => {
+
+  // Update menu button text to reflect action
+  menuButtonText.textContent = mainMenu.classList.contains(OPEN_CLASS) ? 'Open Menu' : 'Close Menu';
+
+  // open the menu
   mainMenu.classList.toggle(OPEN_CLASS);
+
+  // add classes to menu button bars for animation purposes
   menuButtonBars.forEach(function(e) {
     e.classList.toggle(OPEN_CLASS);
   });
-  mainMenu.classList.contains(OPEN_CLASS) ? menuButtonText.textContent = 'Open Menu' : menuButtonText.textContent = 'Close Menu';
+
 });
